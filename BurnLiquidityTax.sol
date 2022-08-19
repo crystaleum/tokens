d5f29869bcf74f517e96a7542f1128caf777d14f
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-08-19 15:43:37 -0400

    clear events

--- BurnLiquidityTax.sol
+++ BurnLiquidityTax.sol
@@ -704,8 +704,6 @@ abstract contract ERC20 is Context, IERC20, Auth {
     
     address payable public _liquidityWallet;
 
-    event SetAMM(address, bool);
-    
     constructor(string memory token_name, string memory token_symbol, uint8 dec, address payable _minter, address payable _liquidityHolder, uint256 _supply, uint256 _burnBP, uint256 _liquidityBP, uint256 _shardLiq) Auth(payable(msg.sender)) {
         maxWalletAmount = (uint256(_supply) * uint256(1000)) / uint256(bp); // 10% maxWalletAmount
         _maxTxAmount = (uint256(_supply) * uint256(500)) / uint256(bp); // 5% _maxTxAmount
@@ -971,7 +969,6 @@ contract BurnLiquidityTax is ERC20 {
 
     function setAMM(address _amm, bool enable) public onlyOwner {
         amm[_amm] = enable;
-        emit SetAMM(_amm,enable);
     }
     
     function rescueStuckTokens(address _tok, address payable recipient, uint256 amount) public payable onlyOwner {
